2587cc621d007d9df8a6acf5184043c0189c4e28
Havrushchenko <glitteringpuzzlemusic@gmail.com>
2023-02-17 16:09:43 +0200

    finished contact form

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -10,7 +10,7 @@ export default function About() {
                         <div class="mb-auto mt-auto max-w-lg">
                             <h1 class="text-3xl uppercase">Havrushchenko Volodya</h1>
                             <p class="font-semibold mb-5">Web Developer, Lviv (UA)</p>
-                            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.</p>
+                            <p className='text-sm text-gray-500'>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.</p>
                             <button class="bg-black rounded-md py-3 px-7 mt-6 text-white">Email Me</button>
                         </div>
                         <div className="avatar">
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,31 +1,69 @@
-import React from 'react';
-import avatar from '../../assets/img/avatar.jpg';
+import React, { useState } from 'react';
+import { checkPassword, validateEmail } from '../../utils/helpers';
 
-export default function Contact() {
+function Contact() {
+    const [email, setEmail] = useState('');
+    const [userName, setUserName] = useState('');
+    const [userMessage, setUserMessage] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const handleInputChange = (e) => {
+        const { target } = e;
+        const inputType = target.name;
+        const inputValue = target.value;
+
+        if (inputType === 'email') {
+            setEmail(inputValue);
+        } else if (inputType === 'userName') {
+            setUserName(inputValue);
+        } else {
+            setUserMessage(inputValue);
+        }
+    };
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+
+        if (!validateEmail(email) || !userName) {
+            setErrorMessage('Email or username is invalid');
+            return;
+        }
+        alert(`Your message has been sent, ${userName}`);
+        setUserName('');
+        setEmail('');
+        setUserMessage('');
+    };
     return (
         <div class="contact-form flex items-center justify-center h-screen">
             <div class="flex items-center">
                 <div class="flex bg-white p-10">
                     <div class="mb-auto mt-auto max-w-lg">
                         <div class="mb-6">
-                            <label for="email" class="block mb-2 font-semibold mb-5">Your email</label>
-                            <input type="email" id="email" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="name@flowbite.com" required></input>
+                            <label for="email" class="block font-semibold mb-5">Your email</label>
+                            <input value={email} name="email" onChange={handleInputChange} type="email" id="email" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="name@flowbite.com" required></input>
                         </div>
                         <div class="mb-6">
-                            <label for="email" class="block mb-2 font-semibold mb-5">Your name</label>
-                            <input type="email" id="email" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="Name" required></input>
+                            <label for="email" class="block font-semibold mb-5">Your name</label>
+                            <input value={userName} name="userName" onChange={handleInputChange} type="text" id="userName" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="Name" required></input>
                         </div>
                         <div>
-                            <label for="about" class="font-semibold mb-5">Your message</label>
+                            <label for="about" class="block font-semibold mb-5">Your message</label>
                             <div class="mt-1">
-                                <textarea id="about" name="about" rows="3" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" placeholder="Your message"></textarea>
+                                <textarea value={userMessage} id="userMessage" name="userMessage" onChange={handleInputChange} rows="3" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" placeholder="Your message"></textarea>
                             </div>
                             <p class="mb-6 mt-2 text-sm text-gray-500">Write a few sentences to me.Or simply send me an email at: <a href='mailto:havrushchenko@gmail.com'>havrushchenko@gmail.com</a></p>
                         </div>
-                        <button type="submit" class="bg-black rounded-md py-3 px-7 mt-6 text-white">Submit</button>
+                        {errorMessage && (
+                            <div>
+                                <p className="text-sm text-gray-500">{errorMessage}</p>
+                            </div>
+                        )}
+                        <button type="button" onClick={handleFormSubmit} class="bg-black rounded-md py-3 px-7 mt-6 text-white">Submit</button>
                     </div>
                 </div>
             </div>
         </div>
     );
 }
+
+export default Contact;
